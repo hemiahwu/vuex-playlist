3e09143fcf56a1d1466a76f42e02c0e3364c5ef1
Nihemiah <Nihemiah@Nihemiah.local>
2019-08-24 13:27:24 +0800

    lesson-3

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
+    <AddTodo />
     <Todos />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 export default {
   name: "app",
-  components: { Todos }
+  components: { Todos, AddTodo }
 };
 </script>
 
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -5,11 +5,18 @@ const getters = {
   getAllTodos: state => state.todos
 };
 const mutations = {
-  setTodos(state, todos) {
-    state.todos = todos;
-  }
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo)
 };
 const actions = {
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      'http://jsonplaceholder.typicode.com/todos',
+      { title, completed: false }
+    );
+    // console.log(response);
+    commit('newTodo', response.data);
+  },
   async fetchTodos({ commit }) {
     const response = await axios.get(
       'http://jsonplaceholder.typicode.com/todos'
