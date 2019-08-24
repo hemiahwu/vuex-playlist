760fb2882cbdb8c8db8ba0c6511bbd09e947ec86
Nihemiah <Nihemiah@Nihemiah.local>
2019-08-24 13:05:52 +0800

    lesson-2

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="todos">代办事项</div>
+  <div>
+    <h3>代办事项</h3>
+    <div class="todos">
+      <div v-for="(todo,index) in getAllTodos" :key="index" class="todo">{{todo.title}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: "todos"
+  name: "todos",
+  computed: mapGetters(["getAllTodos"]),
+  methods: mapActions(["fetchTodos"]),
+  created() {
+    this.fetchTodos();
+  }
 };
 </script>
 
 <style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,7 +1,23 @@
-const state = {};
-const getters = {};
-const mutations = {};
-const actions = {};
+const state = {
+  todos: []
+};
+const getters = {
+  getAllTodos: state => state.todos
+};
+const mutations = {
+  setTodos(state, todos) {
+    state.todos = todos;
+  }
+};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'http://jsonplaceholder.typicode.com/todos'
+    );
+    // console.log(response);
+    commit('setTodos', response.data);
+  }
+};
 
 export default {
   state,
